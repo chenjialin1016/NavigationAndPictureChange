c09a86944c4aea3cd340abad894858c0535fe1a8
陈嘉琳 <chenjialin@chenjialindemacbook-pro.local>
2019-01-02 21:39:25 +0800

    Initial Commit

---
+++ study001-导航栏背景随滑动渐变，往上缩小，往下放大效果/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  study001-导航栏背景随滑动渐变，往上缩小，往下放大效果
+//
+//  Created by 陈嘉琳 on 2019/1/2.
+//  Copyright © 2019年 陈嘉琳. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
